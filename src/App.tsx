e5a6bc67a4079603a2f150053d413e74b9748c63
Hussein Tarek <htarek47@gmail.com>
2024-01-21 15:14:42 +0200

    builder

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { FormBuilder } from "@formio/react";
+function App() {
+  return (
+    <div style={{ padding: "50px" }}>
+      <FormBuilder form={{}} />
+    </div>
+  );
+}
+
+export default App;
